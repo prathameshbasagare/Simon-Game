b4119e41e4dfd2380a790c9baa7733d7d69dc965
prathameshbasagare5 <119438756+prathameshbasagare5@users.noreply.github.com>
2024-02-23 23:20:10 +0530

    Initial commit

---
+++ game.js
@@ -0,0 +1,16 @@
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
+}
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
